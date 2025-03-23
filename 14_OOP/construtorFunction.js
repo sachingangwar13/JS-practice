3f2682913ef4019956d0fccaccd6c1e707f12b24
sachingangwar13 <sss97581300@gmail.com>
2025-03-23 12:28:34 +0530

    Create construtorFunction.js

---
+++ 14_OOP/construtorFunction.js
@@ -0,0 +1,24 @@
+function makeTea(type){
+    this.type = type
+    this.serve = function(){
+        return `your ${this.type} tea is ready.`;
+    };
+}
+
+let masalaTea = new makeTea("masala");
+console.log(masalaTea.serve());
+
+
+// we can also add any property to function using .prototype
+
+        makeTea.prototype.bill = function(){
+            return `Sir your bill of ${this.type} tea is ready.`;
+        }
+
+        let tandooriChai = new makeTea("tandoori");
+        // console.log(tandooriChai.serve());
+
+        console.log(tandooriChai.bill());
+
+console.log(masalaTea.bill());
+
