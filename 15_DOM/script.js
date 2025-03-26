b6c8678bf265e56dd03ae14d1ec3dce8744466e3
sachingangwar13 <sss97581300@gmail.com>
2025-03-26 09:16:28 +0530

    Update script.js

--- 15_DOM/script.js
+++ 15_DOM/script.js
@@ -35,3 +35,34 @@ document.getElementById("removeLastTask").addEventListener('click',function(){
 
     taskList.lastElementChild.remove();
 })
+
+//task 6
+document.getElementById("clickMeButton").addEventListener('click' ,function(){
+    document.getElementById("clickMeButton").classList.add("highlight");
+})
+
+// task 7
+document.getElementById("teaList").addEventListener('click' , function(event){
+    // console.log(event.target);
+    // event.target.classList.add("highlight");
+
+    alert(`you have clicked ${event.target.textContent}`);
+})
+
+// task 8
+document.getElementById("feedbackForm").addEventListener('submit', function(event){
+    event.preventDefault(); // prevents the form from submitting
+    let feedback = document.getElementById("feedbackInput").value;  // get the value of the input
+    document.getElementById("feedbackDisplay").textContent = `feedback is: ${feedback}`;
+})
+
+// task 9
+document.addEventListener('DOMContentLoaded',function(){
+    document.getElementById("domStatus").textContent = "DOM is fully loaded...";
+})
+
+// task 10
+document.getElementById("toggleHighlight").addEventListener('click',function(){
+    let descriptionText = document.getElementById("descriptionText");
+    descriptionText.classList.toggle("highlight");
+})
